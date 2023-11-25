2a43a42be5391cfdb7786bf64f755cb337baa056
Youssef <yg9552753@gmail.com>
2023-11-25 20:18:44 +0200

    Adding getReport() function add test cases

--- creational/Pool/CarPool.php
+++ creational/Pool/CarPool.php
@@ -4,21 +4,24 @@ namespace creational\Pool;
 
 class CarPool
 {
-    private Car $freeCars;
-    private Car $busyCars;
+    private $freeCars = [];
+    private $busyCars = [];
 
     public function rentCar(): Car{
         $car = (empty($this->freeCars)) ? new Car() : array_pop($this->freeCars);
-        $this->busyCars[spl_object_hash()] = $car; // create hash index for the car object created
+        $this->busyCars[spl_object_hash($car)] = $car; // create hash index for the car object created
 
         return $car;
     }
-    public function freeCar(Car $car){
+    public function freeCar(Car $car): void{
         $carId = spl_object_hash($car);
 
         if(isset($this->busyCars[$carId])){
             unset($this->busyCars[$carId]);
             $this->freeCars[$carId] = $car;
         }
+    }
+    public function getReport(): int{
+        return count($this->freeCars) + count($this->busyCars);
     }
 }
